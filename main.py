76231bd88f2a37ebf0cfb96f994a30d34946c21d
Chris Jaros <cjaros1@att.net>
2017-02-23 17:28:14 -0600

    added a way to view all blogs

--- main.py
+++ main.py
@@ -16,29 +16,57 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
 
 
-class MainPage(Handler):
-    def render_front(self, title="", entry="", error=""):
-        entries = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC")
-        self.render("front.html", title=title, entry=entry, error = error, entries = entries)
+class NewPost(Handler):
+    def render_newpost(self, error=""):
+        self.render("newpost.html",  error = error)
     def get(self):
-        self.render_front()
+        self.render_newpost()
     def post(self):
         title = self.request.get("title")
         entry = self.request.get("entry")
         if title and entry:
-            b = Blog(title = title, entry = entry)
+            b = BlogsDB(title = title, entry = entry)
             b.put()
-            self.redirect("/")
+            self.redirect("/blog")
         else:
             error = "we need both a title and blog entry!"
-            self.render_front(title, entry, error)
+            self.render_newpost(error)
 
 
-class Blog(db.Model):
+class Blog(Handler):
+    def render_blog(self):
+        entries = db.GqlQuery("SELECT  * FROM BlogsDB ORDER BY created DESC LIMIT 5")
+        self.render("blog.html",entries=entries)
+    def get(self):
+        self.render_blog()
+
+class Index(Handler):
+    def get(self):
+        self.redirect('/newpost')
+
+class AllBlogs(Handler):
+    def render_all_blogs(self):
+        entries=db.GqlQuery("SELECT * FROM BlogsDB ORDER BY created DESC")
+        self.render("allblogs.html",entries=entries)
+    def get(self):
+        self.render_all_blogs()
+
+class ViewPostHandler(webapp2.RequestHandler):
+    def get(self, id):
+        pass
+
+
+
+class BlogsDB(db.Model):
     title = db.StringProperty(required = True)
     entry = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
 
 
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+app = webapp2.WSGIApplication([('/', Index),
+                               ('/blog', Blog),
+                               ('/newpost', NewPost),
+                               ('/allblogs', AllBlogs),
+                               webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
+                               ], debug=True)
